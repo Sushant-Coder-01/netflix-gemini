3e9290167541b0ae320c3fcc1354f13893141bab
Sushant-Coder-01 <sushantmangore45@gmail.com>
2024-11-06 13:12:31 +0530

    add voice mute/unmute feature

--- src/components/BannerMedia.js
+++ src/components/BannerMedia.js
@@ -1,17 +1,22 @@
 import { useDispatch } from "react-redux";
 import useMovieVideo from "../hooks/useMovieVideo";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { addBannerMovie } from "../redux/moviesSlice";
 
 const BannerMedia = ({ movieId }) => {
   const dispatch = useDispatch();
   const trailerVideo = useMovieVideo(movieId);
+  const [isMuted, setIsMuted] = useState(true); // Initialize mute state
 
   useEffect(() => {
     if (movieId) {
       dispatch(addBannerMovie(movieId));
     }
-  }, [movieId]);
+  }, [movieId, dispatch]);
+
+  const toggleMute = () => {
+    setIsMuted((prevMute) => !prevMute);
+  };
 
   return (
     <div
@@ -20,12 +25,23 @@ const BannerMedia = ({ movieId }) => {
     >
       <iframe
         className="absolute top-0 left-0 w-full h-full object-cover scale-[1.7] lg:scale-[2] lg:-translate-x-1/2 lg:-translate-y-1/2 lg:left-1/2 lg:top-1/2"
-        src={`https://www.youtube.com/embed/${trailerVideo?.key}?controls=0&showinfo=0&modestbranding=0&autoplay=1&mute=1&loop=1&playlist=${trailerVideo?.key}`}
+        src={`https://www.youtube.com/embed/${
+          trailerVideo?.key
+        }?controls=0&showinfo=0&modestbranding=0&autoplay=1&mute=${
+          isMuted ? 1 : 0
+        }&loop=1&playlist=${trailerVideo?.key}`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         sandbox="allow-scripts allow-same-origin allow-presentation"
         allowFullScreen
       ></iframe>
+      {/* Mute/Unmute Button */}
+      <button
+        onClick={toggleMute}
+        className="absolute top-48 md:top-[60%] right-5 md:right-10 p-2 rounded-full bg-gray-700 bg-opacity-70 text-white md:text-xl focus:outline-none cursor-pointer z-50"
+      >
+        {isMuted ? "🔇" : "🔊"}
+      </button>
     </div>
   );
 };
